e6e1be44f8fc82f6983d720d31419f7b3c900c4e
Rotichtonny <tonnyrotich.tr@gmail.com>
2020-04-18 00:14:45 +0300

    Update  Delete Data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else 
+      render 'edit'
+    end
+
   end
 
   def show
@@ -29,6 +38,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
